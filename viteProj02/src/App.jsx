a93901a77e52c80d7824ebb41d2a8462d569a697
HarshitaTripathii <harshitat216@gmail.com>
2025-05-07 16:02:10 +0530

    bgChanger done

--- viteProj02/src/App.jsx
+++ viteProj02/src/App.jsx
@@ -2,22 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './components/Card'
+// import Card from './components/Card'
 import Foot from './components/Foot'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [color, setColor] = useState('gray')
 
   return (
     <>
-    <body className='bg-gray-500'>
-      <Card musicName="Ranjhana" year="2010"/> <Card musicName="Tere Bin" year ="2015"/>
-      <Foot/></body>
-      
 
-   
-    
+      <div className='w-full h-screen duration-200  flex justify-center items-center' style={{ backgroundColor: color }}>
+        <div className='fixed bottom-10 flex justify-evenly w-full h-20'>
+          <Foot setColor={setColor} />
+        </div>
+        {/* <Card musicName="Ranjhana" year="2010" />  */}
+        {/* <Card musicName="Tere Bin" year="2015" /> */}
+
+      </div>
+
+
+
+
+
 
     </>
   )
--- viteProj02/src/components/Foot.jsx
+++ viteProj02/src/components/Foot.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
-function Foot() {
+function Foot({setColor}) {
   return (
-    <div>
-        <div className="flex justify-center items-center w-96 h-22 bg-amber-100 rounded-xl border-2 shadow-xl">
-            <div className="bg-blue-500 w-20 h-16"></div>
-            <div className="bg-red-600 w-20 h-16"></div>
-            <div className="bg-yellow-400 w-20 h- 16"></div>
-            <div className="bg-green-400 w-20 h-16"></div>
-            <div className="bg-black w-20 h-16"></div>
+    // <div className='fixed bottom-10 flex justify-center items-center'>
+        <div className="flex  items-center justify-evenly w-full h-20 bg-transparent rounded-xl  shadow-xl">
+            <button onClick={()=>setColor("blue")} className="bg-blue-500 w-20 h-14 flex rounded-xl  items-center justify-center ">Blue</button>
+            <button onClick={()=>setColor("red")} className="bg-red-600 w-20 h-14 flex rounded-xl  items-center justify-center ">Red</button>
+            <button onClick={()=>setColor("yellow")} className="bg-yellow-400 w-20 h-14 flex rounded-xl  items-center justify-center">Yellow</button>
+            <button onClick={()=>setColor("orange")} className="bg-orange-500 400 w-20 h-14 flex  rounded-xl items-center justify-center">Orange </button>
+            <button onClick={()=>setColor("pink")} className="bg-pink-400 w-20 h-14 flex rounded-xl  items-center justify-center">Pink</button>
+            <button onClick={()=>setColor("green")} className="bg-green-400 w-20 h-14 flex rounded-xl  items-center justify-center ">Green</button>
+            <button onClick={()=>setColor("black")} className="bg-black w-20 h-14 flex rounded-xl  items-center justify-center text-white">Black</button>
         </div>
       
-    </div>
+    // </div>
   )
 }
 
