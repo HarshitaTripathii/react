739c8faaa1e46def7d9e8986bb12eea28649d3da
HarshitaTripathii <harshitat216@gmail.com>
2025-05-18 17:26:55 +0530

    layout of currencyConverter is done finally now

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -9,16 +9,19 @@ function App() {
 
   return (
     <>
-    <div className='flex flex-col justify-center items-center h-screen w-screen '>
-      <div className='h-64 w-[500px] border-2 flex flex-col justify-center relative items-center gap-y-2 border-indigo-600 bg-gradient-to-r from-white/50 to-white/30'>
-      <div className='h-[25px] w-[45px] z-10 absolute top-20 left-1/2 -translate-x-1/2 h-10 w-10 bg-blue-400 border-2 text-center border-blue-700 rounded'>Swap</div>
-      <ToFrom />
-      <ToFrom />
-      <button className='h-[45px] w-[400px] bg-blue-400 rounded-2xl border-3 border-black mt-4'>Convert</button></div>
-      
-     
-    </div>
-      
+      <div className='flex flex-col justify-center items-center h-screen w-screen text-sm '>
+        <div className='h-64 w-[500px] border-2 flex flex-col justify-center relative 
+        items-center gap-y-2  bg-gradient-to-r from-white/50 to-white/30'>
+          <div className='h-[25px] w-[45px] z-10 absolute top-20 
+          left-1/2 -translate-x-1/2 bg-blue-400 border-2 text-center border-blue-700 rounded cursor-pointer'>Swap</div>
+          <ToFrom next="From" />
+          <ToFrom next="To"/>
+          <button className='h-[45px] w-[400px] bg-blue-400 rounded-2xl border-3 border-black mt-4'>Convert</button>
+        </div>
+
+
+      </div>
+
     </>
   )
 }
--- currencyConverter/src/components/ToFrom.jsx
+++ currencyConverter/src/components/ToFrom.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
-function ToFrom() {
+function ToFrom({next}) {
   return (
-    <div className=' h-16 w-[400px] bg-white rounded-2xl border-3 border-black z-1'>
+    <div className=' h-16 w-[400px] bg-white rounded-2xl border-3 border-black z-1 
+    flex justify-between  text-sm'>
+      <div>
+        <div className='flex flex-col ml-3 mt-1 '>
+          <p>{next}</p>
+          <input type='text' placeholder='Enter' className='bg-gray-100 rounded p-1 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500'></input>
+        </div>
+      </div>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col mr-3' >
+          <p>Currency Type</p>
+          <select className='bg-gray-100 rounded p-1 border cursor-pointer border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500'>
+            <option value='usd'>USD</option>
+            <option value='eur'>EUR</option>
+            <option value='inr'>INR</option>
+            <option value='gbp'>GBP</option>
+          </select>
+        </div>
+      </div>
 
-      
+
+
+
     </div>
   )
 }
