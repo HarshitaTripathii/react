7abef02f834b9a7764f0db48fa57b4781bc350eb
HarshitaTripathii <harshitat216@gmail.com>
2025-05-28 10:26:09 +0530

    currency converter created but search box is not present

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -1,40 +1,66 @@
-import { useState , useEffect, useId} from 'react'
+import { useState, useEffect, useId } from 'react'
 import './App.css'
-import {ToFrom} from './components/ToFrom'
+import ToFrom from './components/ToFrom'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 
 function App() {
   // const [count, setCount] = useState(0)
-  const [amt, setAmt]=useState(0)
-  const [from, setFrom]=useState("inr")
-  const [to, setTo]= useState("usd")
+  const [amt, setAmt] = useState(0)
+  const [from, setFrom] = useState("inr")
+  const [to, setTo] = useState("usd")
+  const [convertedAmt, setConvertedAmt] = useState(0);
+
 
   // calling custom hook : useCurrencyInfo : which is returning an Object : data : is an object
-  const currencyInfo=useCurrencyInfo(from)
-  const options=Object.keys(currencyInfo)
- 
+  const currencyInfo = useCurrencyInfo(from)
+  const options = Object.keys(currencyInfo)
+
 
   // for swap button : for swap the from and to men
-  const swap=()=>{
+  const swap = () => {
     setFrom(to)
     setTo(from)
   }
 
   // for convert button :
-  const convert =()=>{
-    
+  const convert = () => {
+    setConvertedAmt(amt * currencyInfo[to]);
+
   }
   return (
     <>
       <div className='flex flex-col justify-center items-center h-screen w-screen text-sm '>
+
         <div className='h-64 w-[500px] border-2 flex flex-col justify-center relative 
         items-center gap-y-2  bg-gradient-to-r from-white/50 to-white/30'>
-          <div onClick={swap} className='h-[25px] w-[45px] z-10 absolute top-20 
-          left-1/2 -translate-x-1/2 bg-blue-400 border-2 text-center border-blue-700 rounded cursor-pointer'>Swap</div>
-          <ToFrom next="From" />
-          <ToFrom next="To"/>
-          <button className='h-[45px] w-[400px] bg-blue-400 rounded-2xl border-3 border-black mt-4'>Convert {from} to {to}</button>
+
+          <div
+            onClick={swap}
+            className='h-[25px] w-[45px] z-10 absolute top-20 
+          left-1/2 -translate-x-1/2 bg-blue-400 border-2 text-center border-blue-700 rounded cursor-pointer'>
+            Swap</div>
+          <ToFrom
+            next="From"
+            amount={amt}
+            currencyOptions={options}
+            onAmountChange={(amt) => setAmt(amt)}
+            selectCurrency={from}
+            onCurrencyChange={(currency) => setFrom(currency)}
+          />
+
+
+          <ToFrom
+            next="To"
+            amount={convertedAmt}
+            currencyOptions={options}
+            selectCurrency={to}
+            onCurrencyChange={(currency) => { setTo((currency)) }}
+
+
+          />
+          <button className='h-[45px] w-[400px] bg-blue-400 rounded-2xl border-3 border-black mt-4'
+            onClick={convert}>Convert {from} to {to}</button>
         </div>
 
 
--- currencyConverter/src/components/ToFrom.jsx
+++ currencyConverter/src/components/ToFrom.jsx
@@ -1,20 +1,35 @@
-import React,{useId} from 'react'
+import React, { useId } from 'react'
 
-function ToFrom({ next, amount, onAmountChange, onCurrencyChange, selectCurrency, currencyOptions }) {
-  const amtId=useId();
+function ToFrom({
+  next,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  selectCurrency = "usd",
+  currencyOptions = [] }) {
+  const amtId = useId();
   return (
     <div className=' h-16 w-[400px] bg-white rounded-2xl border-3 border-black z-1 
     flex justify-between  text-sm'>
       <div>
         <div className='flex flex-col ml-3 mt-1 '>
           <label htmlFor={amtId}>{next}</label>
-          <input type='text' id={amtId} placeholder='Enter' value={amount} onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))} className='bg-gray-100 rounded p-1 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500'></input>
+          <input type='text'
+            id={amtId}
+            placeholder='Enter'
+            value={amount}
+            onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+            className='bg-gray-100 rounded p-1 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500'></input>
         </div>
       </div>
       <div className='flex flex-col justify-center items-center'>
         <div className='flex flex-col mr-3' >
           <p>Currency Type</p>
-          <select value={selectCurrency} onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)} className='bg-gray-100 rounded p-1 border cursor-pointer border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500'>
+          <select
+            value={selectCurrency}
+            onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+            className='bg-gray-100 rounded p-1 border cursor-pointer border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500'>
+
             {currencyOptions.map((currency) => (
               <option key={currency} value={currency}>
                 {currency}
